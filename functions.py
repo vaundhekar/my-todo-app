fcadd12a35df19089102383184d7625b6ae573c3
Vikram Aundhekar <24655595+vaundhekar@users.noreply.github.com>
2024-05-04 19:14:03 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write the to-do item list in th text file.
+    """
+    with open(filepath, 'w') as file:
+        todos_local = file.writelines(todos_arg)
+
+
+if __name__ == '__main__':
+    print("Hello")
+    print(get_todos())
